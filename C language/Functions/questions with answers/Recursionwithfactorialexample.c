ce15a3d734910fb3dcea0e061cb988f558748205
Shivang Dubey <62883921+Shivangdubey049@users.noreply.github.com>
2024-12-02 02:37:46 +0530

    Update Recursionwithfactorialexample.c

--- C language/Functions/questions with answers/Recursionwithfactorialexample.c
+++ C language/Functions/questions with answers/Recursionwithfactorialexample.c
@@ -6,7 +6,7 @@ int fact(int n){                 //Making a function named as fact which contain
     if(n==1 ||n==0)             //Factorial of 1 and 0 is 1 therefore well return the value of 1
     return 1;
     else if(n>1)                //Calculation of factorial when n is bigger than 1 we"ll calc factorial
-    return fact(n-1)*n;//We know 5! will be equal to 4! X 5 and 4! will be 3! X 4 and so on so we write this code
+    return fact(n-1)*n;//We know 5! will be equal to 4! X 5 and 4! will be 3! X 4 and so on hence we write this particular code
 }
 
 void main(){       //Function will be called here
